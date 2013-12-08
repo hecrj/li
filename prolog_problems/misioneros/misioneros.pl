2c69b46ebac78d01777ae8e5fbd100dd02229c51
Héctor <hector0193@gmail.com>
2013-12-08 21:42:51 +0100

    Misioneros fixed.

--- prolog_problems/misioneros/misioneros.pl
+++ prolog_problems/misioneros/misioneros.pl
@@ -1,6 +1,8 @@
 :-include('../solver.pl').
 podar(0).
 
+capacidadBarca(2).
+
 solve:- misioneros(3, 3).
 
 misioneros(M, C):- solucionOptima(mision-i-[M, C]-[0, 0], mision-d-[0, 0]-[M, C]).
@@ -8,23 +10,24 @@ misioneros(M, C):- solucionOptima(mision-i-[M, C]-[0, 0], mision-d-[0, 0]-[M, C]
 coste(L, C):- length(L, C).
 contiene(Estado, Camino):- member(Estado, Camino).
 
-compLado(0, _).
-compLado(M, C):- M >= C.
+ladoEquilibrado(0, _).
+ladoEquilibrado(M, C):- M >= C.
 
 mover(Mo1, Co1, Md1, Cd1, Mo2, Co2, Md2, Cd2):-
-	between(0, 2, Mb),
-	between(0, 2, Cb),
+	capacidadBarca(C),
+	between(0, C, Mb),
+	between(0, C, Cb),
 	B is Mb + Cb,
-	B > 0, B < 3,
+	B > 0, B =< C,
 
 	Mo2 is Mo1 - Mb, Co2 is Co1 - Cb,
-	Mo2 >= 0, Co1 >= 0,
+	Mo2 >= 0, Co2 >= 0,
 
 	Md2 is Md1 + Mb,
 	Cd2 is Cd1 + Cb,
 
-	compLado(Mo2, Co2),
-	compLado(Md2, Cd2).
+	ladoEquilibrado(Mo2, Co2),
+	ladoEquilibrado(Md2, Cd2).
 	
 
 unPaso(mision-i-[Mi1, Ci1]-[Md1, Cd1], mision-d-[Mi2, Ci2]-[Md2, Cd2]):-
